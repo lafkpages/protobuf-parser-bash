cf56404c447e667c495c05b8b5eca340b411e0af
lafkpages <soy.lafk+gh@gmail.com>
2023-07-06 20:35:31 +0200

    🚧 Basic unfinished proto schema lexer

--- src/main.sh
+++ src/main.sh
@@ -11,5 +11,37 @@ if [ -z "$schemaFile" ]; then
   usage
 fi
 
-# Load schema
-schema=$(cat "$schemaFile")
+# Parse schema
+schemaSyntax=""
+schemaParserCurrentToken=""
+schemaParserIsString="0"
+schemaParserTokens=()
+schemaParserOperators=("=" "{" "}" ";")
+while read -n1 char; do
+  if [ "$schemaParserIsString" = "1" ]; then
+    if [ "$char" = "\"" ]; then
+      schemaParserIsString="0"
+      schemaParserTokens+=("$schemaParserCurrentToken\"")
+    fi
+    schemaParserCurrentToken="$schemaParserCurrentToken$char"
+  else
+    if [[ " ${schemaParserOperators[*]} " =~ " $char " ]]; then
+      schemaParserTokens+=("$schemaParserCurrentToken")
+      schemaParserTokens+=("$char")
+      schemaParserCurrentToken=""
+    elif [ "$char" = "\"" ]; then
+      schemaParserIsString="1"
+      schemaParserCurrentToken="\""
+    elif [ "$char" = " " ]; then
+      schemaParserTokens+=("$schemaParserCurrentToken")
+      schemaParserCurrentToken=""
+    else
+      schemaParserCurrentToken="$schemaParserCurrentToken$char"
+    fi
+  fi
+
+  echo "$schemaParserCurrentToken"
+  echo "$schemaParserIsString"
+  echo "$schemaSyntax"
+  echo "${schemaParserTokens[@]}"
+done < "$schemaFile"
