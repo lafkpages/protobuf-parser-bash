e954700aa62001b7ba439d9ca3ff6face86a77c3
lafkpages <soy.lafk+gh@gmail.com>
2023-07-07 03:06:33 +0200

    🚧 Parser get package name from schema

--- src/main.sh
+++ src/main.sh
@@ -26,6 +26,7 @@ printf -- "- %s\n" "${schemaTokens[@]}"
 
 # Interpret tokens
 schemaSyntax=""
+schemaPackage=""
 for i in "${!schemaTokens[@]}"; do
   token="${schemaTokens[$i]}"
   nextToken="${schemaTokens[$i + 1]}"
@@ -58,7 +59,25 @@ for i in "${!schemaTokens[@]}"; do
     fi
 
     schemaSyntax="proto3"
+  else
+    # No other tokens allowed before syntax
+    if [ -z "$schemaSyntax" ]; then
+      echo "Expected 'syntax = \"...\"'" 1>&2
+      exit 1
+    fi
+
+    # Check that the token is package
+    if [ "$token" = "package" ]; then
+      if [ -z "$nextToken" ]; then
+        echo "Expected package name after 'package'" 1>&2
+        exit 1
+      fi
+
+      schemaPackage="$nextToken"
+    fi
   fi
 done
 
-echo "Finished parsing, schema syntax: $schemaSyntax"
+echo "Finished parsing, found:"
+echo "- syntax: $schemaSyntax"
+echo "- package: $schemaPackage"
