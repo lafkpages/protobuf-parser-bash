5909650f4d2ca55f20a7bf1487dd0e6618140c91
lafkpages <soy.lafk+gh@gmail.com>
2023-07-07 04:27:00 +0200

    🚧 Message parser

--- src/interpreter.sh
+++ src/interpreter.sh
@@ -8,6 +8,11 @@ schemaPackage=""
 declare -A schemaMessages
 
 isParsingMessage="0"
+isParsingField="0"
+
+fieldTypes=(string bytes number bool)
+
+skipNextIter="0"
 
 for i in "${!schemaTokens[@]}"; do
   token="${schemaTokens[$i]}"
@@ -27,10 +32,77 @@ for i in "${!schemaTokens[@]}"; do
     continue
   fi
 
-  # If we're parsing a message, check if we've reached the end of it
-  if [ "$token" = "}" ] && [ "$isParsingMessage" = "1" ]; then
-    isParsingMessage="0"
+  # Skip the next iteration if we're supposed to
+  if [ "$skipNextIter" -gt 0 ]; then
+    ((skipNextIter--))
     continue
+  fi
+
+  # If we're parsing a message
+  if [ "$isParsingMessage" = "1" ]; then
+    # Check if we've reached the end of it
+    if [ "$token" = "}" ]; then
+      isParsingMessage="0"
+
+      echo "Parsed message $messageName with fields:"
+      for fieldNumber in "${!messageFieldsNames[@]}"; do
+        echo "- ${messageFieldsTypes["$fieldNumber"]} ${messageFieldsNames["$fieldNumber"]} = $fieldNumber"
+      done
+      echo
+
+      continue
+    fi
+
+    if [ "$isParsingField" = "0" ]; then
+      fieldType="$token"
+
+      # Check that the token is a field type
+      if [[ ! " ${fieldTypes[@]} " =~ " ${fieldType} " ]]; then
+        echo "Expected field type (pos $((i + 1)))" 1>&2
+        exit 1
+      fi
+
+      isParsingField="1"
+      fieldName="$nextToken"
+
+      # Check that the next token is the field name
+      if [[ ! "$fieldName" =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
+        echo "Expected message field name (pos $((i + 1)))" 1>&2
+        exit 1
+      fi
+
+      # Check that the next next token is an equals sign
+      if [ "$nextNextToken" != "=" ]; then
+        echo "Expected '=' after message field name (pos $((i + 2)))" 1>&2
+        exit 1
+      fi
+
+      # Check that the next next next token is a number
+      if [[ ! "$nextNextNextToken" =~ ^[0-9]+$ ]]; then
+        echo "Expected number after message field '=' (pos $((i + 3)))" 1>&2
+        exit 1
+      fi
+
+      fieldNumber="$nextNextNextToken"
+
+      # Check that the next next next next token is a semicolon
+      if [ "$nextNextNextNextToken" != ";" ]; then
+        echo "Expected ';' after message field number (pos $((i + 4)))" 1>&2
+        exit 1
+      fi
+
+      # Save field
+      messageFieldsTypes["$fieldNumber"]="$fieldType"
+      messageFieldsNames["$fieldNumber"]="$fieldName"
+
+      # Skip next tokens
+      skipNextIter="3"
+    elif [ "$token" = ";" ]; then
+      isParsingField="0"
+    else
+      echo "This should never happen" 1>&2
+      exit 1
+    fi
   fi
 
   if [ "$token" = "syntax" ]; then
@@ -92,7 +164,12 @@ for i in "${!schemaTokens[@]}"; do
 
       isParsingMessage="1"
 
-      declare -A messageFields
+      unset messageFieldsTypes messageFieldsNames
+      declare -A messageFieldsTypes
+      declare -A messageFieldsNames
+
+      # Skip next token
+      skipNextIter="2"
 
       ;;
     esac
@@ -100,6 +177,6 @@ for i in "${!schemaTokens[@]}"; do
 done
 
 # Unset variables that are no longer needed
-unset isParsingMessage
-unset messageName
-unset messageFields
+unset isParsingMessage messageName messageFieldsTypes \
+  messageFieldsNames skipNextIter fieldTypes fieldType \
+  fieldName fieldNumber
