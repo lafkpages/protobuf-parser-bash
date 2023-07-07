90f5afae7e422fb37de0d8514ddce2eab4ac6e17
lafkpages <soy.lafk+gh@gmail.com>
2023-07-07 13:49:20 +0200

    🏷️ Interpreter add all types

--- src/interpreter.sh
+++ src/interpreter.sh
@@ -16,7 +16,23 @@ isParsingMessage="0"
 isParsingEnum="0"
 isParsingField="0"
 
-fieldTypes=(string bytes int32 bool)
+fieldTypes=(
+  double
+  float
+  int32
+  int64
+  uint32
+  uint64
+  sint32
+  sint64
+  fixed32
+  fixed64
+  sfixed32
+  sfixed64
+  bool
+  string
+  bytes
+)
 
 skipNextIter="0"
 
