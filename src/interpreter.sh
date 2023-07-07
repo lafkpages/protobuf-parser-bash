20ada086c2f03d63ac054bf28bab3b390b7b9ca7
lafkpages <soy.lafk+gh@gmail.com>
2023-07-07 03:13:28 +0200

    🚚 Move interpreter to its own file

---
+++ src/interpreter.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# This is meant to be sourced from src/main.sh
+
+schemaSyntax=""
+schemaPackage=""
+
+for i in "${!schemaTokens[@]}"; do
+  token="${schemaTokens[$i]}"
+  nextToken="${schemaTokens[$i + 1]}"
+  nextNextToken="${schemaTokens[$i + 2]}"
+  nextNextNextToken="${schemaTokens[$i + 3]}"
+
+  if [ "$token" = "syntax" ]; then
+    # Check that the next token is an equals sign
+    if [ "$nextToken" != "=" ]; then
+      echo "Expected '=' after 'syntax'" 1>&2
+      exit 1
+    fi
+
+    # Check that the next next next token is a string
+    if [ "${nextNextToken:0:1}" != "\"" ] || [ "${nextNextToken: -1}" != "\"" ]; then
+      echo "Expected string after 'syntax ='" 1>&2
+      exit 1
+    fi
+
+    # Check that the next next next token is a semicolon
+    if [ "$nextNextNextToken" != ";" ]; then
+      echo "Expected ';' after 'syntax = \"...\"'" 1>&2
+      exit 1
+    fi
+
+    # Check that the syntax is proto3
+    if [ "$nextNextToken" != "\"proto3\"" ]; then
+      echo "Only proto3 syntax is supported" 1>&2
+      exit 1
+    fi
+
+    schemaSyntax="proto3"
+  else
+    # No other tokens allowed before syntax
+    if [ -z "$schemaSyntax" ]; then
+      echo "Expected 'syntax = \"...\"'" 1>&2
+      exit 1
+    fi
+
+    # Check that the token is package
+    if [ "$token" = "package" ]; then
+      if [ -z "$nextToken" ]; then
+        echo "Expected package name after 'package'" 1>&2
+        exit 1
+      fi
+
+      schemaPackage="$nextToken"
+    fi
+  fi
+done
