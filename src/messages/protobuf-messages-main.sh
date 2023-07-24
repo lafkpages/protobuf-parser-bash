a7d80ccdbd0ef4d82ad03f36e9543eb6e7921aa4
lafkpages <soy.lafk+gh@gmail.com>
2023-07-24 17:00:45 +0900

    ✨ Messages main script

---
+++ src/messages/protobuf-messages-main.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo "Usage: $0" 1>&2
+  exit 1
+}
+
+# Call the lexer on the message
+echo "Lexing message..."
+source ./src/messages/protobuf-messages-lexer.sh
+
+# Log lexer results
+echo "Finished lexing, found ${#messageTokens[@]} tokens:"
+printf -- "- %s\n" "${messageTokens[@]}"
+echo
