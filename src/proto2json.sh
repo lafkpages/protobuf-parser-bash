2a5460d6bc82bc0853a6f3b28257bfa454fe4bca
lafkpages <soy.lafk+gh@gmail.com>
2023-07-25 11:58:37 +0900

    ✨ proto2json (from Bashval)

---
+++ src/proto2json.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# This is a simpler utility for converting the result of decoding from `protoc`
+# into JSON. This does not need a schema, so might return wrongly structured
+# data sometimes, but it works as a simple and quick solution in most cases.
+
+json='{'
+
+isInString=0
+isAfterColon=0
+shouldSkipNlComma=0
+isQuotingEnum=0
+
+currentToken=""
+
+while IFS="" read -n1 char; do
+  if [ "$isInString" = 1 ]; then
+    currentToken="$currentToken$char"
+
+    if [ "$char" = '"' ]; then
+      isInString=0
+    fi
+  else
+    if [ "$isQuotingEnum" = 1 ] && [[ ! "$char" =~ [a-zA-Z_] ]]; then
+      isQuotingEnum=0
+      currentToken="$currentToken\""
+    fi
+
+    if [ "$char" = "\"" ]; then
+      isInString=1
+      currentToken="$currentToken$char"
+    elif [ "$char" = " " ]; then
+      :
+    elif [ "$char" = "" ]; then
+      if [ "$shouldSkipNlComma" = 1 ]; then
+        json="$json$currentToken"
+        shouldSkipNlComma=0
+      else
+        json="$json$currentToken,"
+      fi
+      currentToken=""
+    elif [ "$char" = ':' ]; then
+      json="$json\"$currentToken\":"
+      currentToken=""
+      isAfterColon=1
+    elif [ "$char" = "{" ]; then
+      json="$json\"$currentToken\":{"
+      currentToken=""
+      shouldSkipNlComma=1
+    elif [ "$char" = "}" ]; then
+      json="${json%,}}"
+      currentToken=""
+    else
+      if [ "$isAfterColon" = 1 ]; then
+        currentToken="$currentToken\"$char"
+        isQuotingEnum=1
+      else
+        currentToken="$currentToken$char"
+      fi
+    fi
+  fi
+
+  if [ "$char" = ":" ] || [ "$char" = " " ]; then
+    :
+  else
+    isAfterColon=0
+  fi
+done
+
+json="${json%,}}"
+
+jsonWithDupes=$(jq -Mnc --stream -f src/utils/dupekeys.jq <<<"$json")
+
+if [ "$?" = 0 ]; then
+  echo "$jsonWithDupes"
+else
+  echo "$json" 1>&2
+  exit "$?"
+fi
