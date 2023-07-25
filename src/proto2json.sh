4bef5b52f40ad317d1def19fab03779b21ba0c59
lafkpages <soy.lafk+gh@gmail.com>
2023-07-25 12:19:24 +0900

    ✨ proto2json get script path when loading dupekeys JQ

--- src/proto2json.sh
+++ src/proto2json.sh
@@ -69,7 +69,16 @@ done
 
 json="${json%,}}"
 
-jsonWithDupes=$(jq -Mnc --stream -f src/dupekeys.jq <<<"$json")
+# Get the path of this script
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+
+jsonWithDupes=$(jq -Mnc --stream -f "$DIR/dupekeys.jq" <<<"$json")
 
 if [ "$?" = 0 ]; then
   echo "$jsonWithDupes"
